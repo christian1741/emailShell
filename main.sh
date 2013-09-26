d6c41c921b2f068abd922a07241b8e17dd22ffc6
christian1741 <christian1741@users.noreply.github.com>
2013-09-25 20:06:40 -0400

    Update and rename meetingreminder.sh to main.sh

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 #Code by Christian Caicedo 
 #Start your code from here
 
-RutaEV="your_path/data.data"    #Path data.data  - > list of users with the event details
-RutaEmail="your_path/FriendlyRemainderEmail.email"    #Path FriendlyRemainderEmail.email -> Message to be mailed. 
+PathEV="your_path/data.data"    #Path data.data  - > list of users with the event details
+PathEmail="your_path/FriendlyRemainderEmail.email"    #Path FriendlyRemainderEmail.email -> Message to be mailed. 
 
 Month=$(date +%m) #It takes the currently month 
 Day=$(date +%d) #It takes the currently day
@@ -11,15 +11,15 @@ Day=$(date +%d) #It takes the currently day
 while IFS="," read Event FirstName LastName MeetingMonth MeetingDay EmailAddress
 do
 #IFS=$oIFS
- 	if [ "$MeetingMonth" == "$Mes" ] && [ "$MeetingDay" == "$Dia" ]
+ 	if [ "$MeetingMonth" == "$Month" ] && [ "$MeetingDay" == "$Day" ]
 	then
 		name=$FirstName
 		lastN=$LastName
 		email=$EmailAddress
 		Meeting=$Event
 		Full_Name= $FirstName $LastName 
-sed 's/__FULLNAME__/$Full_Name/g' $RutaEmail | mail -s "Meeting on: $Month $Day" $email
+sed 's/__FULLNAME__/$Full_Name/g' $PathEmail | mail -s "Meeting on: $Month $Day" $email
 
 fi
 
-done < $RutaEV
+done < $PathEV
